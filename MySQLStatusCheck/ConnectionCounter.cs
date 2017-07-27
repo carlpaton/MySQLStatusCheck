ca2c54cfe106d012923b1b69ade4092751ef70a3
carlp <carlp@grindrodbank.co.za>
2017-07-27 14:34:56 +0200

    initial

---
+++ MySQLStatusCheck/ConnectionCounter.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace MySQLStatusCheck
+{
+    public class ConnectionCounter
+    {
+        private readonly MySqlConnection connection;
+
+        public ConnectionCounter()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnMySql"].ConnectionString);
+        }
+
+        public void DoConnect()
+        {
+            Open();
+            string stop = "";
+        }
+
+        public void DoConnectWithUsing()
+        {
+            using (connection)
+            {
+                Open();
+                string stop = "";
+            }
+        }
+
+        private void Open()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
---
+++ MySQLStatusCheck/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLStatusCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SHOW STATUS WHERE `variable_name` = 'Threads_connected';
+            //show session status;
+
+
+            int connections = 1500;
+
+
+
+
+            try
+            {
+                while (connections > 0)
+                {
+                    //new ConnectionCounter().DoConnect();
+                    new ConnectionCounter().DoConnectWithUsing();
+                    connections--;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            string stop = "";
+        }
+    }
+}
